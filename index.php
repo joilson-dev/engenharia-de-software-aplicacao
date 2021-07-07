9ffd15f1e6652ccda2e13c85ece2d033aaccf42f
Fabrício Louzada Colodete <74249168+fabricio2001@users.noreply.github.com>
2021-07-07 07:23:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,15 @@ include(DIRREQ . "helpers/variables.php");
 $diespatch = new Classes\ClassDispatch();
 include($diespatch->getInclusão());
 */
-
+/*
 mysqli_connect("us-cdbr-east-04.cleardb.com", "b5da11fde38f6a", "44b2e8bf", "heroku_3762ce4dc37b2bb") or die(mysqli_error());
 echo "Connected to MySQL<br />";
+*/
+
+include("config/config.php");
+try {
+  $con = new \PDO("mysql:host=" . HOST . ";dbname=" . DB . "", "" . USER . "", "" . PASS . "");
+  echo "Conectou no Banco de dados";
+} catch (\PDOException $erro) {
+  echo "Deu algum problema";
+}
