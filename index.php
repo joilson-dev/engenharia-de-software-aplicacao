e213092002c88bb067ac1aff273b9e188f098ec5
Fabrício Louzada Colodete <74249168+fabricio2001@users.noreply.github.com>
2021-07-07 07:13:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 use Classes\ClassDispatch;
 
 header("Content-Type: text/html; charset=utf-8");
@@ -9,4 +9,7 @@ include(DIRREQ . "helpers/variables.php");
 
 $diespatch = new Classes\ClassDispatch();
 include($diespatch->getInclusão());
+*/
 
+mysqli_connect("us-cdbr-east-04.cleardb.com", "b5da11fde38f6a", "44b2e8bf", "heroku_3762ce4dc37b2bb") or die(mysqli_error());
+echo "Connected to MySQL<br />";
