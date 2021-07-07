f3a75589082ce769e0443991cc7d1981ff4a5c7a
Fabrício Louzada Colodete <74249168+fabricio2001@users.noreply.github.com>
2021-07-07 15:07:12 -0300

    Update ClassConexao.php

--- models/ClassConexao.php
+++ models/ClassConexao.php
@@ -7,9 +7,9 @@ abstract class ClassConexao
     protected function conectaDB()
     {
         try {
-            $con = new PDO("mysql:host=" . HOST . ";dbname=" . DB . "", "" . USER . "", "" . PASS . "");
+            $con = new \PDO("mysql:host=" . HOST . ";dbname=" . DB . "", "" . USER . "", "" . PASS . "");
             return $con;
-        } catch (PDOException $erro) {
+        } catch (\PDOException $erro) {
             return $erro->getMessage();
         }
     }
