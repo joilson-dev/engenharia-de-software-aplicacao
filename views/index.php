9f562f3ff7c1a4d1bee23e4ecbdefbb96e37516d
Fabio Louzada Colodtte <85516788+Fabio-L-C@users.noreply.github.com>
2021-07-09 08:18:44 -0300

    Update index.php
    Atualização do index.php

--- views/index.php
+++ views/index.php
@@ -1,6 +1,13 @@
 <?php \Classes\ClassLayout::setHead('Homepage', 'Esse e o homepage do nosso site'); ?>
-    <h1>HOMEPAGE</h1>
-    <a href="<?php echo DIRPAGE . 'cadastro' ?>">Cadastro</a><br>
-    <a href="<?php echo DIRPAGE . 'login' ?>">Login</a>
+<div id="homepage">
+    <div id="homemenu" class="center">
+        <h1 style="margin: 10px;">TRABALHO DE ENGENHARIA DE SOFTWARE</h1>
+        <br>
+        <a href="<?php echo DIRPAGE . 'cadastro' ?>">Cadastro</a><br>
+        <br>
+        <a href="<?php echo DIRPAGE . 'login' ?>">Login</a>
 
+        <p>Fabício, Fábio, Joilson</p>
+    </div>
+</div>
 <?php \Classes\ClassLayout::setFooter(); ?>
